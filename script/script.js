680b8c297a9f7bc4d78989d408eefe90e6a532c2
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-10-29 00:10:14 -0300

    Tentativa nº1 (Ainda ajustando)

---
+++ script/script.js
@@ -0,0 +1,28 @@
+
+// Tarefa Exemplo
+
+/*let nomeCliente = prompt("Digite o nome do cliente");
+let qtdPaes = parseInt(prompt("Quantas unidades de pães ?"));
+
+let valorCompra = parseFloat(qtdPaes * 0.75);
+
+alert(`Valor da compra é: ${valorCompra.toFixed(2)}`);
+
+let data = Date.now();
+let hoje = new Date(data).toUTCString();
+
+alert(
+  `${nomeCliente} - Valor da compra: ${valorCompra} - Produto: Pães - Quantidade: ${qtdPaes} - Data: ${hoje} `
+);*/
+
+
+// Primeira Tarefa
+
+let numberOne = parseInt(prompt("Digite o primeiro número inteiro"));
+let numberTwo = parseInt(prompt("Digite o segundo número inteiro"));
+
+let maiorNumero = (numberOne > numberTwo);
+let divisao = (numberTwo % numberOne);
+
+alert(`O número maior é o ${maiorNumero} e a diferença é ${divisao}`);
+
